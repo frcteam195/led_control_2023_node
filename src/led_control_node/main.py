9f2e19c814021ef44f0d40d4005681dda443dffe
Tom <TSLourenco@WPI.edu>
2023-01-20 22:04:02 +0000

    Testing Rob's new CANdle API.

--- src/led_control_node/main.py
+++ src/led_control_node/main.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
 
-import tf2_ros
 import rospy
 from threading import Thread
 
 from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode, BufferedROSMsgHandlerPy
 from ck_ros_msgs_node.msg import Led_Control
+from ck_utilities_py_node.led import LEDColor, LEDControlMode, LEDStrip, LEDStripType, RGBWColor
 
 def ros_func():
-    control_sub = BufferedROSMsgHandlerPy(Led_Control)
-    control_sub.register_for_updates("LedControl")
+    control_subscriber = BufferedROSMsgHandlerPy(Led_Control)
+    control_subscriber.register_for_updates("LedControl")
+
+    test_timer = 0
 
     rate = rospy.Rate(20)
 
     rospy.loginfo("LED")
+    test_strip = LEDStrip(0, LEDStripType.RGBW)
+    red = LEDColor(RGBWColor(R=255), 0, 8)
+    green = LEDColor(RGBWColor(G=255), 0, 8)
+    blue = LEDColor(RGBWColor(B=255), 0, 8)
+
     while not rospy.is_shutdown():
 
-        if control_sub.get() is not None:
+        if control_subscriber.get() is not None:
 
-            if control_sub.get().control_mode == control_sub.get().SET_LED:
+            if control_subscriber.get().control_mode == Led_Control.SET_LED:
                 #Set_LED Code
                 pass
             else:
                 #Animate Code
                 pass
 
-        
+        # Set LEDs as a Test Every 5 Seconds or so...
+        if test_timer > 100:
+            test_strip.setLEDControlMode(LEDControlMode.Static)
+            test_strip.setLEDColor(red)
+            test_timer = 0
+
+        test_timer += 1
 
         rate.sleep()
 
